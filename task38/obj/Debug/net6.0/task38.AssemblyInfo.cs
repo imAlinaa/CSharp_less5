926e4cde20c8bc168dfc3fe5dbe2813cee2dc3f2
imAlinaa <alina.btkv@yandex.ru>
2022-12-24 17:43:44 +0700

    task38

--- task38/obj/Debug/net6.0/task38.AssemblyInfo.cs
+++ task38/obj/Debug/net6.0/task38.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("less5")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("task38")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("less5")]
-[assembly: System.Reflection.AssemblyTitleAttribute("less5")]
+[assembly: System.Reflection.AssemblyProductAttribute("task38")]
+[assembly: System.Reflection.AssemblyTitleAttribute("task38")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
